03d438d703d2cf458fc2ffb923760a3d0cf65e74
Thomas Griesmayer <griesmayer@spengergasse.at>
2019-04-08 19:28:09 +0200

    Setup database connection

---
+++ src/connection/OracleConnection.java
@@ -0,0 +1,45 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import domain.BankAccountException;
+
+public class OracleConnection {
+
+	private Connection con;
+	
+	public OracleConnection() throws BankAccountException {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+		}
+		catch ( ClassNotFoundException e) {
+			throw new BankAccountException("Could not find database driver!");
+		}
+	}
+	
+	public void open() throws BankAccountException {
+		try {
+			con = DriverManager.getConnection("jdbc:oracle:thin:localhost:1521:ORCL", "DEMO", "oracle");
+			con.setAutoCommit(false);
+		}
+		catch ( SQLException e) {
+			throw new BankAccountException("Could not open database connection!");
+		}
+	}
+	
+	public void close() throws BankAccountException {
+		try {
+			con.close();
+		}
+		catch ( SQLException e) {
+			throw new BankAccountException("Could not close database connection!");
+		}
+	}
+	
+	public Connection getConnection() {
+		return con;
+	}
+	
+}
